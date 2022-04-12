8c0f91cec3042fdbcbdaee1924cf3ac6245b6bc1
vandergail <biggs@vandergail.com>
2022-04-12 12:09:43 -0400

    layout and css updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,51 @@
-module.exports = {
-	dir: {
-		input: "src",
-		output: "docs"
+const moment = require('moment');
+
+moment.locale('en');
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addWatchTarget("./src/css/");
+	eleventyConfig.addWatchTarget("./src/images/");
+	eleventyConfig.addPassthroughCopy("./src/css/");
+	eleventyConfig.addPassthroughCopy("./src/images/");
+
+	eleventyConfig.addFilter('dateIso', date => { return moment(date).toISOString();});
+	eleventyConfig.addFilter('dateReadable', date => { return moment(date).utc().format('LL');});
+
+	eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+
+	return {
+		dir: {
+			input: "src",
+			output: "docs"
+		},
+		markdownTemplateEngine: "njk"
 	}
 };
+
+function extractExcerpt(article) {
+	if (!article.hasOwnProperty('templateContent')) {
+	  console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+	  return null;
+	}
+   
+	let excerpt = null;
+	const content = article.templateContent;
+   
+	// The start and end separators to try and match to extract the excerpt
+	const separatorsList = [
+	  { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+	  { start: '<p>', end: '</p>' }
+	];
+   
+	separatorsList.some(separators => {
+	  const startPosition = content.indexOf(separators.start);
+	  const endPosition = content.indexOf(separators.end);
+   
+	  if (startPosition !== -1 && endPosition !== -1) {
+		excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+		return true; // Exit out of array loop on first match
+	  }
+	});
+   
+	return excerpt;
+  }
---
+++ src/posts/2022/post-1.md
@@ -0,0 +1,14 @@
+---
+layout: layouts/base.njk
+title: First Post!
+date: 2022-04-12
+tags: ['post']
+---
+<!-- Excerpt Start -->
+Hi there!
+
+Welcome to my website hosted via GitHub Pages. I'm using [eleventy](https://www.11ty.dev/) to generate my webpage. Highly recommended!
+<!-- Excerpt End -->
+I look forward to sharing information about various projects I am currently working on as well as other future interests. I may also throw out random thoughts related to anything I feel worthy of sharing.
+
+Hope you will find my small abode on the web interesting!
